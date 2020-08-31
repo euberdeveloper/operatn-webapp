f4569562ff01f3bdea4c5bdc861c49750980fa18
Matteo Padovan <mpadovan@live.it>
2020-08-31 10:51:06 +0200

    aggiunti controlli al form - fix

--- src/views/sportello/Bollette.vue
+++ src/views/sportello/Bollette.vue
@@ -5,12 +5,6 @@ v-card
   v-card-text(style="padding-bottom: 0px; padding-top: 0px;")
     v-subheader.pa-0 Ricerca per id contratto/nome, data di inizio o per data di scadenza
   v-card-text
-<<<<<<< Updated upstream
-    v-radio-group(v-model="radios" :mandatory="true")
-      v-radio(value="idcontratto" label="Id contratto o nome")
-      v-radio(value="datainizio" label="Data di inizio")
-      v-radio(value="datascadenza" label="Data di scadenza")
-=======
     v-radio-group(v-model="radios", :mandatory="true")
       v-radio(value="idcontratto", label="Id contratto")
       v-radio(
@@ -22,7 +16,6 @@ v-card
         label="Contratti che terminano nel periodo (da selezionare)"
       )
       v-radio(value="evoluto", label="Ricerca avanzata") 
->>>>>>> Stashed changes
     v-expand-transition
       v-form.mb-2(v-show="expand[0]")
         v-text-field(
@@ -35,78 +28,6 @@ v-card
     v-expand-transition
       v-form.mb-2(v-show="expand[1]")
         v-row.mx-2.mt-2
-<<<<<<< Updated upstream
-          v-col
-            input-date-picker(
-                  dense,
-                  label="Dal",
-                  required
-                )
-          v-col
-            input-date-picker(
-                  dense,
-                  label="Al",
-                  required
-                )
-    v-expand-transition
-      v-form.mb-2(v-show="expand[2]")
-        v-row.mx-2.mt-2
-          v-col
-            input-date-picker(
-                  dense,
-                  label="Dal",
-                  required
-                )
-          v-col
-            input-date-picker(
-                  dense,
-                  label="Al",
-                  required
-                )
-    v-data-table#print.elevation-1(:headers='headers', :items='contratti', dense='')
-      template(v-slot:item.controls='props')
-        v-btn.mx-2(small='', color='primary', @click='dettaglio(props.item)')
-          v-icon(dark='') mdi-card-account-mail
-    v-card-text
-      v-checkbox(v-model="evoluto" label="Evoluto")
-      v-row.mx-2.mt-2
-          v-col
-            input-date-picker(
-                  dense,
-                  label="Dal",
-                  required
-                )
-          v-col
-            input-date-picker(
-                  dense,
-                  label="Al",
-                  required
-                )
-      v-row.mx-2.mt-2
-          v-col
-            v-select(:items="items" label="Tipo di contratto")
-          v-col
-            v-select(:items="items" label="Tipo di utente")
-      v-row.mx-2.mt-2
-          v-col
-            v-select(:items="items" label="Anno contratto")
-          v-col
-            input-date-picker(
-                  dense,
-                  label="Dal",
-                  required
-                )
-            input-date-picker(
-                  dense,
-                  label="Al",
-                  required
-                )
-    v-card-text  
-      v-btn(color='primary', @click='') Continua
-      v-btn(text, @click='') Pulisci campi
-    
-
-=======
           v-col.py-0
             v-card-text(v-if="!error") Seleziona l'intervallo di date
             v-card-text(v-else, style="color: red") {{error}}
@@ -194,7 +115,6 @@ v-card
     v-card-text 
       v-btn(color="primary", @click="submitCampi") Continua
       v-btn(text, @click="clearCampi") Pulisci campi
->>>>>>> Stashed changes
 </template>
 
 <script>
@@ -202,30 +122,6 @@ import Vue from "vue";
 import { mapState } from "vuex";
 // import { mapState, mapActions } from 'vuex'
 export default {
-<<<<<<< Updated upstream
-    data() {
-        return {
-            evoluto: null,
-            bollette: null,
-            radios: null,
-            id_contratto: null,
-            expand: [false, false, false]
-        }
-    },
-    watch: {
-      radios() {
-        this.expand = [false, false, false];
-        switch (this.radios) {
-          case "idcontratto":
-            this.expand[0] = true;
-            break;
-          case "datainizio":
-            this.expand[1] = true;
-            break;
-          case "datascadenza":
-            this.expand[2] = true;
-            break;
-=======
   data() {
     return {
       error: false,
@@ -325,28 +221,9 @@ export default {
                 console.log(error);
               }
             );
->>>>>>> Stashed changes
         }
       });
     },
-<<<<<<< Updated upstream
-    computed: {
-      headers() {
-        let h = ['nome', 'cognome', 'sesso', 'descrizione_fabbricato', 'inizio contratto', 'fine contratto', 'check in', 'check out'].map(x => { return {text: x, value: x } })
-        h.push({ text: "", value: "controls", sortable: false })
-        return h
-      }
-    }
-
-  //   computed: {
-	// 		...mapState('alloggi',[]),
-	// 	},
-		
-	// 	methods: {
-	// 		...mapActions('alloggi', []),
-	// 	},
-}
-=======
     clearCampi() {
       this.submitted = false;
       this.error = false;
@@ -357,5 +234,4 @@ export default {
     this.$store.dispatch("inserimentoContratto/loadTipiContratti");
   },
 };
->>>>>>> Stashed changes
 </script>
