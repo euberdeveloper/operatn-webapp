6377d16fcc5d0579dbe41bcc00b18f1002975dd5
euberdeveloper <euberdeveloper@gmail.com>
2021-05-15 21:42:17 +0200

    feat(tabellone): remove vertical from tabs
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/views/root/tabellone/RootTabelloneView.vue
+++ src/views/root/tabellone/RootTabelloneView.vue
@@ -6,7 +6,7 @@
     </v-toolbar>
 
     <!-- TABS -->
-    <v-tabs v-model="activeTab" :vertical="$vuetify.breakpoint.lgAndUp">
+    <v-tabs v-model="activeTab">
       <v-tab v-for="tab of tabs" :key="tab.path" :to="tab.path">
         <v-icon left>{{ tab.icon }}</v-icon>
         <span>{{ tab.label }}</span>
