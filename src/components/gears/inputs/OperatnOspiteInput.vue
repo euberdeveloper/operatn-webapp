1b3db0433fe66f123e896f5deabec247959ac1ce
euberdeveloper <euberdeveloper@gmail.com>
2021-05-19 10:24:41 +0200

    fix: fix ospite input validator
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/components/gears/inputs/OperatnOspiteInput.vue
+++ src/components/gears/inputs/OperatnOspiteInput.vue
@@ -34,14 +34,14 @@ import OspiteHandlerMixin from "@/mixins/handlers/OspiteHandlerMixin";
 export default class OperatnOspiteInput extends Mixins(OspiteHandlerMixin) {
   /* PROPS */
 
-  @Prop({ validator: (v) => typeof v === "number" || v === null, required: true })
+  @Prop({ validator: (v) => typeof v === "number" || v === null || v === undefined, required: true })
   value!: number | null;
 
   @Prop({ type: Number, default: 10 })
   pageSize!: number;
 
-  @Prop({ type: String, required: true })
-  label!: string;
+  @Prop({ type: String, required: false })
+  label?: string;
 
   @Prop({ type: String, required: false })
   icon?: string;
