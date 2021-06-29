fcfcb9f9f5766da2c34a56095a365661bd98bcb4
euberdeveloper <euberdeveloper@gmail.com>
2021-06-29 19:18:18 +0200

    FUCK VA DA DIO
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,12 @@
 
 echo "Build site"
 npm run build
-rm -r node_modules src public
+rm -rf node_modules src public
 
 echo "Removing any pre-existing static file"
 cd site
-rm -r *
+rm -rf *
 cd ..
 
 echo "Passing all the new static files from dist to site"
-cp dist/* site -r
+cp dist/* site -rf
